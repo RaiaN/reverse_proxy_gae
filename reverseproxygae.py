dc75acf2c15f1b95937ae301a24b767605b32f73
RaiaN <leontev.petr@gmail.com>
2016-04-08 16:41:58 +0300

    Fix

--- reverseproxygae.py
+++ reverseproxygae.py
@@ -7,24 +7,40 @@ from StringIO import StringIO
 import gzip
 
 required = set(
-    ['user-agent', 'accept', 'accept-encoding', 'accept-language',
-     'cache-control', 'pragma', 'expires', 'host', 'content-encoding'
+    ['accept', 'accept-encoding', 'accept-language', 'connection',
+     'user-agent',
+     'cache-control', 'pragma', 'expires', 'content-encoding',
      'x-serialize-format', 'x-gs-cookie', 'x-unity-version', 'x-gs-accept']
 )
+
+def return_basic_headers():
+    h = (
+        {
+            'User-Agent': '0.1-dev/WindowsEditor/0.2/itunes',
+            'Accept': 'protocol/GSAPI.1, format/json',
+            'X-Serialize-Format': 'json'
+        }
+    )
+    h['X-GS-Cookie'] = (
+        'device_identifer=%s;account_type=guest;\
+         account_id=guest;\
+         account_secure=799a1ea440671c73d1124dd0b288df62' % 'Kong6F2D775D10266339A4A0C148F587774D'
+    )
+    return h
 
 
 class ProxyHandler(webapp2.RequestHandler):
     def post(self, *args, **kwargs):
-        # print(self.request.path)
-        # print(self.request.body)
-        # print(self.request.headers)
-        request_headers = dict(
-            (k.lower(), v) for k, v in self.request.headers.items()
-        )
-        true_headers = (
-            ((key, request_headers[key])
-             for key in request_headers if key.lower() in required)
-        )
+        if "ping" in self.request.path:
+            true_headers = return_basic_headers()
+        else:
+            request_headers = dict(
+                (k.lower(), v) for k, v in self.request.headers.items()
+            )
+            true_headers = (
+                ((key, request_headers[key])
+                 for key in request_headers if key.lower() in required)
+            )
         target_url = 'http://dev.tinyarmypanoramic.appspot.com/%s' % self.request.path
         response = fetch(
              target_url, payload=self.request.body, method="POST",
@@ -33,12 +49,7 @@ class ProxyHandler(webapp2.RequestHandler):
         self.response.content_type = response.headers["Content-Type"]
         self.response.status = response.status_code
         self.response.headers = response.headers
-        self.response.headers['Cache-Control'] = (
-            'no-cache, must-revalidate'
-        )
-        self.response.headers['Pragma'] = 'no-cache'
-        self.response.headers['Expires'] = 'Thu, 01 Dec 1994 16:00:00'
-        print("TEST")
+
         print(response.content)
         print(response.headers)
 
@@ -47,7 +58,7 @@ class ProxyHandler(webapp2.RequestHandler):
             gzip_f = gzip.GzipFile(fileobj=buf)
             content = gzip_f.read()
         else:
-            content = response.body
+            content = response.content
         self.response.out.write(content)
 
 
