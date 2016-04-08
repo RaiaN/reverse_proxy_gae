31e806a1025ac922a09488e6abb1d8b8470aab89
RaiaN <leontev.petr@gmail.com>
2016-04-08 15:52:18 +0300

    Fix

--- reverseproxygae.py
+++ reverseproxygae.py
@@ -1,7 +1,7 @@
 from google.appengine.api.urlfetch import fetch
 
-import cgi
-import datetime
+# import cgi
+# import datetime
 import webapp2
 
 
@@ -13,23 +13,23 @@ required = set(
 
 
 class ProxyHandler(webapp2.RequestHandler):
-    def post(self):
+    def post(self, *args, **kwargs):
         request_headers = dict(self.request.headers.items())
         true_headers = (
             ((key, request_headers[key])
              for key in request_headers if key in required)
         )
         target_url = 'http://tinyarmypanoramic.appspot.com/%s' % self.request.path
-        # response = fetch(
-        #      target_url, payload=self.request.data, method="POST",
-        #      headers=dict(true_headers)
-        # )
-        # self.response.content_type = response.headers["Content-Type"]
-        # self.response.status = response.status_code
-        # self.response.headers = response.headers
-        self.response.out.write("TEST") # response.content)
+        response = fetch(
+             target_url, payload=self.request.body, method="POST",
+             headers=dict(true_headers)
+        )
+        self.response.content_type = response.headers["Content-Type"]
+        self.response.status = response.status_code
+        self.response.headers = response.headers
+        self.response.out.write(response.content)
 
 
 app = webapp2.WSGIApplication([
-    webapp2.Route(r'/<:.*>', handler=ProxyHandler),
+    webapp2.Route(r'/<:.*>', handler=ProxyHandler)
 ])
