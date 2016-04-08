61a764ce00bec6fcddb3433b82b2300bb2626c30
RaiaN <leontev.petr@gmail.com>
2016-04-08 16:50:59 +0300

    Fix

--- reverseproxygae.py
+++ reverseproxygae.py
@@ -44,22 +44,13 @@ class ProxyHandler(webapp2.RequestHandler):
         target_url = 'http://dev.tinyarmypanoramic.appspot.com/%s' % self.request.path
         response = fetch(
              target_url, payload=self.request.body, method="POST",
-             headers=dict(true_headers), deadline=65
+             headers=dict(true_headers)
         )
         self.response.content_type = response.headers["Content-Type"]
         self.response.status = response.status_code
         self.response.headers = response.headers
 
-        print(response.content)
-        print(response.headers)
-
-        if self.response.headers.get('Content-Encoding') == 'gzip':
-            buf = StringIO.StringIO(self.response.read())
-            gzip_f = gzip.GzipFile(fileobj=buf)
-            content = gzip_f.read()
-        else:
-            content = response.content
-        self.response.out.write(content)
+        self.response.out.write(response.content)
 
 
 app = webapp2.WSGIApplication([
