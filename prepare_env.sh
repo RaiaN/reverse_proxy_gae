5d7d33a939ea0256176ce6a3b49da76d0b798e19
RaiaN <leontev.petr@gmail.com>
2016-04-12 19:31:24 +0300

    Script to setup swift env

---
+++ prepare_env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export OS_USER_ID=dab7700c6e96438b8ae75f1f4bebdcdf
+export OS_PASSWORD=HII2sV^WeVd0X,{7
+export OS_PROJECT_ID=acc5ebaa5c6649aaa6a30e88e17f5df6
+export OS_AUTH_URL=https://lon-identity.open.softlayer.com/v3
+export OS_REGION_NAME=london
+export OS_IDENTITY_API_VERSION=3
+export OS_AUTH_VERSION=3
